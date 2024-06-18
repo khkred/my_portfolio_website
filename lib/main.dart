7a6003a7025ce026d91de5865c222816efff50c4
Harish Kunchala <32921476+khkred@users.noreply.github.com>
2024-06-18 11:44:08 -0700

    added logos

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
+    return MaterialApp.router(
       title: 'Harish Kunchala',
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
